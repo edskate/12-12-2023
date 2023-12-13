8af74a5b61913a194594e25c0c22a0d3e768bf60
Edilson gomes de lima <edskateicevida@gmail.com>
2023-12-13 01:48:13 -0300

    Fundamentos javascript

---
+++ scripts.js
@@ -0,0 +1,49 @@
+console.log('hello world');
+console.log('esta é uma menssagem ')
+
+
+
+// variaveis 
+var x = 10
+// maneiras mais modernas
+let y = 15
+const z = 20
+
+if (1) {
+    let y = 55
+    console.log(y)
+}
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+y = 25
+
+console.log(y)
+
+// tipos de dados
+
+const name = 'Matheus'
+console.log(name)
+console.log(typeof name)
+
+const shirtsQty = 4
+console.log(shirtsQty)
+console.log(typeof shirtsQty)
+
+const decimal = 12.4
+
+console.log(decimal)
+console.log(typeof decimal)
+const isApproved = false // true ou false
+console.log(isApproved)
+console.log(typeof isApproved)
+
+let surname = null
+console.log(surname)
+console.log(typeof surname)
+
+surname = 'battisti'
+console.log(surname)
+console.log(typeof surname)
