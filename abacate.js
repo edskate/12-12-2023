fe1eb47b11b3e1f4e468c0cc2460c3ee3ad02623
Edilson gomes de lima <edskateicevida@gmail.com>
2023-12-13 02:22:45 -0300

    add

--- abacate.js
+++ abacate.js
@@ -5,3 +5,9 @@ console.log("Hello abacate")
 
 console.log(abacate)
 abacate = 20
+
+
+function multipleNumber(number1, number2){
+    return number1 * number2;
+}
+let result =  multipleNumber(3, 2));
